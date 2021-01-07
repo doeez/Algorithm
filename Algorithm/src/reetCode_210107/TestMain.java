29db307ed901a9f8c74020fa4e41e457e28e7f14
dohxxzun <dohxxzun@DESKTOP-F51RRH5>
2021-01-07 18:40:11 +0900

    gitignore

--- Algorithm/src/reetCode_210107/TestMain.java
+++ Algorithm/src/reetCode_210107/TestMain.java
@@ -8,12 +8,25 @@ import java.io.OutputStreamWriter;
 
 public class TestMain {
 	public static void main(String[] args) {
+		/*
 		try {
 			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 			int n = Integer.parseInt(br.readLine());
 			for(int i = 1; i <= n; i++) {
 				bw.write(i+"\n");
+			}
+			bw.flush();
+		} catch (IOException e) { //
+			e.printStackTrace();
+		}
+		*/
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+			int n = Integer.parseInt(br.readLine());
+			for(int i = 0; i < n; i++) {
+				bw.write((n-i)+"\n");
 			}
 			bw.flush();
 		} catch (IOException e) { //
